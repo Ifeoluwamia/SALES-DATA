7bffdc77265ed3d477993c575cb8f6f1f7a6b9fa
Ifeoluwa Aruleba <Ifeoluwayomikun@gmail.com>
2024-11-09 12:12:25 +0100

    Update Sales_analysis _queries.sql

--- Sales_analysis _queries.sql
+++ Sales_analysis _queries.sql
@@ -36,6 +36,12 @@ GROUP BY Orderdate;
 
 --find the top 5 customers by total purchase amount.
 
+SELECT Region, SUM(Total_Sales) AS RegionSales
+FROM [dbo].[Sales data]
+GROUP BY Region;
+
+--calculate the percentage of total sales contributed by each region
+
 SELECT Region, SUM(Total_Sales) AS Regionalsales,
 (SUM(Total_Sales) / CAST((SELECT SUM(Total_Sales) FROM [dbo].[Sales data])
 AS DECIMAL(10,2)) * 100) AS SalesPercentage
@@ -43,24 +49,8 @@ FROM [dbo].[Sales data]
 GROUP BY Region;
 
 
---calculate the percentage of total sales contributed by each region
-
-SELECT Region, (SUM(Total_sales) AS RegionalSales,
-SUM (Total_Sales) / CAST((SELECT SUM(Total_sales) FROM [dbo].[Sales data])
-AS DECIMAL (10, 2)) * 100) AS SalesPercentage
-FROM [dbo].[sales data]
-GROUP BY Region;
-
-
-
-SELECT Region, SUM(Total_Sales) AS RegionSales
-FROM [dbo].[Sales data]
-GROUP BY Region
-
-
 --identify products with no sales in the last quarter.
 
 SELECT DISTINCT Product FROM [dbo].[Sales data]
 WHERE Product NOT IN (SELECT Product FROM [dbo].[Sales data]
 WHERE OrderDate >= DATEADD (QUARTER, -1, GETDATE()));
-
